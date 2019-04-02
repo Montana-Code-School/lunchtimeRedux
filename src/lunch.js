303324c9e70b753736ca6d74977333054df89b10
megalobamboozlementarianism <abe.coley@gmail.com>
2019-04-02 14:03:56 -0600

    initial commit lunchthing

---
+++ src/lunch.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Lunch extends Component {
+
+  render() {
+    return (
+      <div className="Lunch">
+        <p>for lunch i ate {this.props.lunch} </p>
+        <p>after i at the {this.props.lunch}, i was thirsty and I drank a {this.props.drink}</p>
+        <p>and me and courtney {this.props.chitchat}.</p>
+      </div>
+    );
+  }
+}
+
+export default Lunch;
